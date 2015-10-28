c82290cc1d0ab94edc0d8606f357189825e9d739
Rob Napier <robnapier@gmail.com>
2015-10-28 11:11:56 -0400

    Remove redundant finals (thinks to Howard Lovatt). Add copyright/public domain notice.

--- MyAnySequence.playground/Contents.swift
+++ MyAnySequence.playground/Contents.swift
@@ -80,7 +80,7 @@ Here's one possible approach. The cost is a little memory (for `box`) and a litt
 */
 
 final class MyAnyGenerator<Element> : _AnyGeneratorBase, GeneratorType, SequenceType {
-    private final var box: AnyGenerator_<Element>? = nil
+    private var box: AnyGenerator_<Element>? = nil
     override init() {}
     init<G : GeneratorType where G.Element == Element>(_ base: G) {
         box = _GeneratorBox(base)
@@ -233,7 +233,7 @@ So why did Apple do it with all these boxes and wrappers and abstract tomfoolery
 */
 
 public final class AnyGenerator__<Element> : GeneratorType, SequenceType {
-    private final let _next: () -> Element?
+    private let _next: () -> Element?
     public init<G : GeneratorType where G.Element == Element>(var _ base: G) {
         _next = { return base.next() }
     }
@@ -268,3 +268,19 @@ History suggests that when Apple's implementation is dramatically more complicat
 
 And of course the possibility remains that I got important parts of this wrong. We'll know when stdlib is open source.
 */
+
+//: # Copyright and public domain notice
+//: The text of the article, marked in /*: */ blocks, is Copyright 2015 by Rob Napier.
+//:
+//: Except where otherwise indicated, all source code in this article is public domain:
+//:
+//: This is free and unencumbered software released into the public domain.
+//:
+//: Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.
+//:
+//: In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
+//:
+//: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//:
+//: For more information, please refer to <http://unlicense.org/>
+
